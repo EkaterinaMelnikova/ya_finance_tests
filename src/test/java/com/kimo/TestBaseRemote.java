5b9e8671775c08d0e86960ca62990a25a50c371a
EkaterinaMelnikova <katkimo@gmail.com>
2025-11-01 00:13:47 +0300

    @BeforeEach added to TestBaseRemote

--- src/test/java/com/kimo/TestBaseRemote.java
+++ src/test/java/com/kimo/TestBaseRemote.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -27,7 +28,10 @@ public class TestBaseRemote {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    void addListener(){
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
